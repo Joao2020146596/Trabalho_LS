dd4f1694a43edd60d349265beff6e80fbb592cbe
Joao2020146596 <a2020146596@isec.pt>
2026-05-29 00:25:44 +0100

    Fix da duplicação de ficheiros

--- Trabalho_LS/src/components/footer/footer.component.jsx
+++
@@ -1,2 +0,0 @@
-import React from 'react';
-export default function Footer() { return <footer><p>DEIS @ ISEC</p></footer>; }
--- Trabalho_LS/src/components/header/header.component.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export default function Header() {
-  return <header><h1 className="title">Batalha Naval Avançada</h1><h2 className="subtitle">LS 2025/2026</h2></header>;
-}
